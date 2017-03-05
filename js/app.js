273ff96c8b7d388e5d6c2dd3cdadd3b8dae0c111
Vol4n3 <jcvolan@gmail.com>
2017-03-05 23:09:55 +0100

    Add header

---
+++ js/app.js
@@ -0,0 +1,19 @@
+(function(){
+    
+    let offset500;
+
+    window.addEventListener('scroll',function(e){
+        let ht = document.getElementById('header_title')
+        if(window.pageYOffset > 500 && !offset500){
+            ht.style.left = "500px";
+            ht.style.opacity = "0";
+            offset500 = true;
+        }
+        if(window.pageYOffset < 500 && offset500){
+            ht.style.left = "0px";
+            ht.style.opacity = "1";
+            offset500 = false;
+        }
+        document.getElementById('main_header').style.backgroundPositionY = window.pageYOffset/5 *-1 + 'px';
+    })
+})()
