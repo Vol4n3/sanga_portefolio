cd1edab1b456f29dfd6ecc521fb64836567c256f
Vol4n3 <jcvolan@gmail.com>
2017-03-09 23:11:19 +0100

    Fix font

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@
 
     window.addEventListener('scroll',function(e){
         let ht = document.getElementById('header_title')
-        if(window.pageYOffset > 500 && !offset500){
+        if(window.pageYOffset > 300 && !offset500){
             ht.style.left = "350px";
             ht.style.opacity = "0";
             offset500 = true;
         }
-        if(window.pageYOffset < 500 && offset500){
+        if(window.pageYOffset < 300 && offset500){
             ht.style.left = "0px";
             ht.style.opacity = "1";
             offset500 = false;
