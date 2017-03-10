feaaca10519fb9511b9f662263acfb82c73a2883
Julien COEURVOLAN <Julien COEURVOLAN>
2017-03-10 13:07:38 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-(function(){
-    
+(function () {
+
     let offset500;
 
-    window.addEventListener('scroll',function(e){
+    window.addEventListener('scroll', function (e) {
         let ht = document.getElementById('header_title')
-        if(window.pageYOffset > 300 && !offset500){
+        if (window.pageYOffset > 300 && !offset500) {
             ht.style.left = "350px";
             ht.style.opacity = "0";
             offset500 = true;
         }
-        if(window.pageYOffset < 300 && offset500){
+        if (window.pageYOffset < 300 && offset500) {
             ht.style.left = "0px";
             ht.style.opacity = "1";
             offset500 = false;
         }
-        document.getElementById('main_header').style.backgroundPositionY = window.pageYOffset/15 *-1 + 'px';
-    })
+        document.getElementById('main_header').style.backgroundPositionY = window.pageYOffset / 15 * -1 + 'px';
+    });
+
 })()
