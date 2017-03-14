f54fe4bd4bc9741250f48e14c2f0214c2b72c267
Vol4n3 <jcvolan@gmail.com>
2017-03-14 22:35:12 +0100

    add song

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
 (function(){
-    
-    let offset500;
-
+    let isMoved;
+    document.getElementById('main_header').style.backgroundPositionY = window.pageYOffset/10 *-1 + 'px';
     window.addEventListener('scroll',function(e){
         let ht = document.getElementById('header_title')
-        if(window.pageYOffset > 300 && !offset500){
+        if(window.pageYOffset > 150 && !isMoved){
             ht.style.left = "350px";
             ht.style.opacity = "0";
-            offset500 = true;
+            isMoved = true;
         }
-        if(window.pageYOffset < 300 && offset500){
+        if(window.pageYOffset < 150 && isMoved){
             ht.style.left = "0px";
             ht.style.opacity = "1";
-            offset500 = false;
+            isMoved = false;
         }
-        document.getElementById('main_header').style.backgroundPositionY = window.pageYOffset/15 *-1 + 'px';
-    })
+        document.getElementById('main_header').style.backgroundPositionY = window.pageYOffset/100*-1 + 'px';
+    });
+    
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var context = new AudioContext();
+    var audioBuffer = null;
+    function load(){
+        var req = new XMLHttpRequest();
+        req.open('get','assets/extrait.mp3');
+        req.responseType = 'arraybuffer';
+        req.addEventListener('load',function(ev){
+            context.decodeAudioData(req.response,function(buffer){
+                audioBuffer = buffer;
+                var source = context.createBufferSource();
+                source.buffer = buffer;                   
+                source.connect(context.destination);     
+                source.start(0);   
+            })
+        });
+        req.send();
+    }
+    load();
 })()
