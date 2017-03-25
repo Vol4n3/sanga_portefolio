7f185f855a78055fc9081af91676d9224ee45d19
Vol4n3 <jcvolan@gmail.com>
2017-03-26 00:59:22 +0100

    add new style

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var context = new AudioContext();
     var analyseur = context.createAnalyser();
-    analyseur.fftSize = 32768;
+    analyseur.fftSize = 2048;
+    //analyseur.fftSize = 1024;
     var audioBuffer = null;
     function load() {
         var req = new XMLHttpRequest();
@@ -18,6 +19,7 @@
                 var source = context.createBufferSource();
                 var gainNode = context.createGain();
                 source.buffer = buffer;
+                source.connect(analyseur);
                 source.connect(analyseur);
                 analyseur.connect(gainNode);
                 gainNode.connect(context.destination);
@@ -32,10 +34,10 @@
 
     // particles
     /**
-     * @type {HTMLCanvasElement}
+     * @type {HTMLCanvasElement} canvas
      */
-     var mainHeader = document.getElementById('main_header');
     var canvas = document.getElementById('particle');
+    var mainHeader = document.getElementById('main_header');
     canvas.width = mainHeader.offsetWidth;
     canvas.height = mainHeader.offsetHeight;
     window.addEventListener('resize', function () {
@@ -54,44 +56,60 @@
     var tableauDonnees = new Uint8Array(tailleBuffer);
     w.speed = 0.50;
     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    
-            var freq = 135;
-            var sfreq = 35;
+
+    var freq = 600;
+    var sfreq = 0;
     function boucle() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         w.draw(ctx);
-       
+
         analyseur.getByteFrequencyData(tableauDonnees);
         ctx.beginPath();
         ctx.fillStyle = 'rgb(255, 255, 255)';
         ctx.strokeStyle = 'rgb(255, 255,255)';
         ctx.lineWidth = 2;
-        var sliceWidth = canvas.width * 1.0 / tailleBuffer;
-        var largeurBarre = (canvas.width / (tailleBuffer/5)) * 2.5;
+        var largeurBarre = (canvas.width / (tailleBuffer / 2     )) * 2.5;
         var hauteurBarre;
         var x = sfreq;
         var basseCount = 0;
-
-        for(var i = sfreq; i<freq; i++ ){
-            basseCount += tableauDonnees[0];
-        }
-        var moyenn = basseCount / (freq - sfreq);
-        var boost =(moyenn  /20 +0.50 );
-        if(moyenn >45){
-
-            w.speed = boost +0.50;
-        }else{
-                w.speed = 0.50;
-        }
-      for (var i = sfreq; i < freq  ; i++) {
-          hauteurBarre = tableauDonnees[i];
-          ctx.fillStyle = 'rgb(' + hauteurBarre + ',' + hauteurBarre + ',' + hauteurBarre + ')';
-          ctx.fillRect(x, canvas.height - hauteurBarre, largeurBarre, hauteurBarre);
-          x += largeurBarre + 1;
-      }
-        ctx.lineTo(canvas.width, canvas.height / 2);
-        ctx.stroke();
+        /*
+                for(var i = sfreq; i<freq; i++ ){
+                    basseCount += tableauDonnees[0];
+                }
+                var moyenn = basseCount / (freq - sfreq);
+                var boost =(moyenn  /20 +0.50 );
+                if(moyenn >45){
         
+                    w.speed = boost +0.50;
+                }else{
+                        w.speed = 0.50;
+                }
+              for (var i = sfreq; i < freq  ; i++) {
+                  hauteurBarre = tableauDonnees[i];
+                  ctx.fillStyle = 'rgb(' + hauteurBarre + ',' + hauteurBarre + ',' + hauteurBarre + ')';
+                  ctx.fillRect(x, canvas.height - hauteurBarre, largeurBarre, hauteurBarre);
+                  x += largeurBarre + 1;
+              }
+                ctx.lineTo(canvas.width, canvas.height / 2);
+                ctx.stroke();
+                */
+                ctx.beginPath();
+        ctx.moveTo(x, canvas.height -tableauDonnees[0]);
+        var i;
+        var pas = 1;
+        for (i = sfreq; i < freq - 2; i+=pas) {
+            x += largeurBarre + pas;
+            var xc = (x +  x + largeurBarre +pas ) / 2;
+            var yc = ( tableauDonnees[i] + tableauDonnees[i + pas]) / 2;
+   
+                ctx.quadraticCurveTo(x, canvas.height -tableauDonnees[i], xc, canvas.height -yc);
+
+            
+            
+        }
+        // curve through the last two points
+        //ctx.quadraticCurveTo(x,  tableauDonnees[i], x + largeurBarre + pas,0);
+        ctx.stroke();
         requestAnimationFrame(boucle)
     }
     requestAnimationFrame(boucle)
